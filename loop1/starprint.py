ff348c856865016f62a54d9459df2244b56adca8
farjanahossain <farjana.96.hossain@gmail.com>
2021-04-16 13:38:23 +0600

    star

---
+++ loop1/starprint.py
@@ -0,0 +1,44 @@
+# *
+# * *
+# * * *
+# * * * *
+number = int(input('Enter number of rows : '))
+i = 1
+while i <= number:
+    j = 1
+    while j <= i:
+        print("*", end=" ")
+        j += 1
+    print()
+    i += 1
+
+
+# * * * *
+# * * *
+# * *
+# *
+number = int(input('Enter number of rows : '))
+i = 1
+while i <= number:
+    j = number
+    while j >= i:
+        print("*", end=" ")
+        j -= 1
+    print()
+    i += 1
+
+
+# * * * *
+# * * * *
+# * * * *
+# * * * *
+
+number = int(input('Enter number of rows : '))
+temp = number
+while number > 0:
+    count = temp
+    while count > 0:
+        print("*", end=" ")
+        count -= 1
+    print()
+    number -= 1
